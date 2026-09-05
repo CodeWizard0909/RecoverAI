effa39092b9d29812234915f0c3222cf0c5de1e8
CodeWizard0909 <shivangchaudhary789@gmail.com>
2026-09-05 19:28:01 +0530

    chore: removed scratch test scripts from github tracking

--- test_rzp2.py
+++
@@ -1,7 +0,0 @@
-import os
-import razorpay
-from dotenv import load_dotenv
-
-load_dotenv('.env.local')
-rzp_client = razorpay.Client(auth=(os.environ['RAZORPAY_KEY_ID'], os.environ['RAZORPAY_KEY_SECRET']))
-print(dir(rzp_client))
--- test_tool.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-sys.path.append('python-agent')
-from agent import create_recovery_link
-
-print(create_recovery_link('3a5244f9-e370-4cad-867c-12f688302bca', 75000, False, 'test'))
